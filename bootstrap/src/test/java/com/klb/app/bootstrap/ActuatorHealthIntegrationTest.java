364b1416233732932e8caeb46850274cc34293ee
Vu Hoang <vuhv48@gmail.com>
2026-04-27 08:07:12 +0700

    CICD fix build failed

--- bootstrap/src/test/java/com/klb/app/bootstrap/ActuatorHealthIntegrationTest.java
+++ bootstrap/src/test/java/com/klb/app/bootstrap/ActuatorHealthIntegrationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,6 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Import(TestCacheConfig.class)
 @ActiveProfiles("test")
 class ActuatorHealthIntegrationTest {
 
